ff25e4c88508527bf2744eaf2084ddc0bdf3ed72
Andres1~ <andrescorredor20@gmail.com>
2024-02-18 17:43:15 -0500

    Update user authentication logic to use bcrypt for password hashing

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,10 @@ registerCoreBlocks();
 const App = () => {
   return (
 
-    <div style={{ width: "100%", height: "100vh" }} >
-      <header className="w-full bg-black text-white">
+    <div style={{ width: "100%", height: "100vh" }}>
+      <div className="absolute w-full bg-[#FFD1F3] h-[800px] z-[-10]">
+      </div>
+      <header className="w-full bg-black text-white ">
         <p className="h-10 font-sans text-right text-lg ">
           Call us: (855) 962-5303
         </p>
@@ -17,147 +19,154 @@ const App = () => {
           Today Only: Sign Up and Save $10—With Speedy, Free Shipping
         </div>
       </header>
+
       <div className="flex flex-col items-center">
-        <img src="logo192.png" alt="logo" />
-        <Form
-          formId={1}
-          applyLogic={true}
-          formObj={{
-            blocks: [
-              {
-                name: "welcome-screen",
-                id: "jg1401r",
-                attributes: {
-                  label: "Weight loss made easy with a prescription to semaglutide",
-                  description: "Lose up to 1.5% of your body fat per week"
+        <div className="h-[160px] flex">
+          <img src="logo192.png" alt="logo" className="object-cover h-auto w-full" />
+        </div>
+
+        <div className="w-full h-[32rem]">
+          <Form
+            formId={1}
+            applyLogic={true}
+            formObj={{
+              blocks: [
+                {
+                  name: "welcome-screen",
+                  id: "jg1401r",
+                  attributes: {
+                    label: "Weight loss made easy with a prescription to semaglutide",
+                    description: "Lose up to 1.5% of your body fat per week"
+                  }
+                },
+                {
+                  name: "short-text",
+                  id: "kd12edg",
+                  attributes: {
+                    layout: "split-right",
+                    required: true,
+                    label: "Let's start with your name"
+                  }
+                },
+                {
+                  name: "slider",
+                  id: "93pda11",
+                  attributes: {
+                    label: "Please choose your weight",
+                    min: 0,
+                    max: 100,
+                    step: 1
+                  }
+                },
+                {
+                  name: "number",
+                  id: "wer3qdkdb",
+                  attributes: {
+                    required: true,
+                    label: "Great, can you type your age?"
+                  }
+                },
+                {
+                  name: "dropdown",
+                  id: "3nsdf934",
+                  attributes: {
+                    choices: [
+                      {
+                        label: "Choice 1",
+                        value: "choice-1"
+                      },
+                      {
+                        label: "Choice 2",
+                        value: "choice-2"
+                      }
+                    ]
+                  }
+                },
+                {
+                  name: "long-text",
+                  id: "m35612edg",
+                  attributes: {
+                    required: true,
+                    label: "Type a brief about yourself!"
+                  }
+                },
+                {
+                  name: "date",
+                  id: "a213rsew",
+                  attributes: {
+                    required: true,
+                    label: "Please type your birth of date!"
+                  }
+                },
+                {
+                  name: "multiple-choice",
+                  id: "gqr1294c",
+                  attributes: {
+                    required: true,
+                    multiple: true,
+                    verticalAlign: false,
+                    label: "Which subjects do you love the most?",
+                    choices: [
+                      {
+                        label: "Physics",
+                        value: "physics"
+                      },
+                      {
+                        label: "Math",
+                        value: "math"
+                      },
+                      {
+                        label: "English",
+                        value: "english"
+                      },
+                      {
+                        label: "Biology",
+                        value: "biology"
+                      }
+                    ]
+                  }
+                },
+                {
+                  name: "statement",
+                  id: "g91imf1023",
+                  attributes: {
+                    label: "You are doing great so far!",
+                    buttonText: "Continue",
+                    quotationMarks: true
+                  }
                 }
+              ],
+              settings: {
+                animationDirection: "vertical",
+                disableWheelSwiping: false,
+                disableNavigationArrows: false,
+                disableProgressBar: false
               },
-              {
-                name: "short-text",
-                id: "kd12edg",
-                attributes: {
-                  layout: "split-right",
-                  required: true,
-                  label: "Let's start with your name"
-                }
-              },
-              {
-                name: "slider",
-                id: "93pda11",
-                attributes: {
-                  label: "Please choose your weight",
-                  min: 0,
-                  max: 100,
-                  step: 1
-                }
-              },
-              {
-                name: "number",
-                id: "wer3qdkdb",
-                attributes: {
-                  required: true,
-                  label: "Great, can you type your age?"
-                }
-              },
-              {
-                name: "dropdown",
-                id: "3nsdf934",
-                attributes: {
-                  choices: [
-                    {
-                      label: "Choice 1",
-                      value: "choice-1"
-                    },
-                    {
-                      label: "Choice 2",
-                      value: "choice-2"
-                    }
-                  ]
-                }
-              },
-              {
-                name: "long-text",
-                id: "m35612edg",
-                attributes: {
-                  required: true,
-                  label: "Type a brief about yourself!"
-                }
-              },
-              {
-                name: "date",
-                id: "a213rsew",
-                attributes: {
-                  required: true,
-                  label: "Please type your birth of date!"
-                }
-              },
-              {
-                name: "multiple-choice",
-                id: "gqr1294c",
-                attributes: {
-                  required: true,
-                  multiple: true,
-                  verticalAlign: false,
-                  label: "Which subjects do you love the most?",
-                  choices: [
-                    {
-                      label: "Physics",
-                      value: "physics"
-                    },
-                    {
-                      label: "Math",
-                      value: "math"
-                    },
-                    {
-                      label: "English",
-                      value: "english"
-                    },
-                    {
-                      label: "Biology",
-                      value: "biology"
-                    }
-                  ]
-                }
-              },
-              {
-                name: "statement",
-                id: "g91imf1023",
-                attributes: {
-                  label: "You are doing great so far!",
-                  buttonText: "Continue",
-                  quotationMarks: true
-                }
+              theme: {
+                font: "Roboto",
+                buttonsBgColor: "#9b51e0",
+                logo: {
+                  src: ""
+                },
+                questionsColor: "#000",
+                answersColor: "#0aa7c2",
+                buttonsFontColor: "#fff",
+                buttonsBorderRadius: 25,
+                errorsFontColor: "#fff",
+                errorsBgColor: "#f00",
+                progressBarFillColor: "#000",
+                progressBarBgColor: "#ccc"
               }
-            ],
-            settings: {
-              animationDirection: "vertical",
-              disableWheelSwiping: false,
-              disableNavigationArrows: false,
-              disableProgressBar: false
-            },
-            theme: {
-              font: "Roboto",
-              buttonsBgColor: "#9b51e0",
-              logo: {
-                src: ""
-              },
-              questionsColor: "#000",
-              answersColor: "#0aa7c2",
-              buttonsFontColor: "#fff",
-              buttonsBorderRadius: 25,
-              errorsFontColor: "#fff",
-              errorsBgColor: "#f00",
-              progressBarFillColor: "#000",
-              progressBarBgColor: "#ccc"
-            }
-          }}
-          onSubmit={(data, { completeForm, setIsSubmitting }) => {
-            setTimeout(() => {
-              setIsSubmitting(false);
-              completeForm();
-            }, 500);
-          }}
-        />
+            }}
+            onSubmit={(data, { completeForm, setIsSubmitting }) => {
+              setTimeout(() => {
+                setIsSubmitting(false);
+                completeForm();
+              }, 500);
+            }}
+          />
+        </div>
+
         <img src="brands.png" alt="logo" />
         <div>
           <h3>Experienced U.S. Licensed Doctors</h3>
