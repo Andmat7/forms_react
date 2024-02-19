80764dccb189fe4ef6a7014cd85e4884d88f3ac6
Andres1~ <andrescorredor20@gmail.com>
2024-02-19 01:19:15 -0500

    Update Opinions and Doctors components

--- src/components/init_page/Doctors.tsx
+++ src/components/init_page/Doctors.tsx
@@ -1,13 +1,13 @@
 export function Doctors() {
   return (
-    <div className="bg-[#EDF3FE] font-azo w-full">
+    <div className="bg-[#EDF3FE] font-azo w-full m-[15px]">
       <div className="bg-[#60A5F8] h-[50px]">
         <h3 className="font-bold text-white text-[21px] ml-[90px] h-[50px] leading-[50px]">Experienced U.S. Licensed Doctors</h3>
       </div>
 
       <div className="flex justify-end">
         <div className="ml-[90px] w-[340px] ">
-          <p className=" font-italic  text-[14px]">
+          <p className="font-italic text-[14px] mt-[30px]">
             “I firmly believe that managing weight should be straightforward and accessible for anyone who seeks it. That’s why our program at SkinnyRx is exceptional—you can receive FDA-approved, clinically-proven semaglutide treatment delivered directly to your doorstep at a fraction of the usual cost.”
           </p>
         </div>
