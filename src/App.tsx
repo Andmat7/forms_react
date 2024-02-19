6f7c935514c06e1fb1889b072f71ded6af28ccd8
Andres1~ <andrescorredor20@gmail.com>
2024-02-19 00:34:25 -0500

    Refactor Opinion component and add customer review

--- src/App.tsx
+++ src/App.tsx
@@ -34,11 +34,21 @@ const App = () => {
         <FormSemaglutide />
         <img src="brands.png" alt="logo" />
         <Doctors />
-        <Opinion />
+        <div className="bg-[#F7F7F7] text-center p-[64px]">
+          <h2>Believe the Hype:</h2>
+          <h2>Customer Success with Clinically</h2>
+          <h2>Proven Meds</h2>
+          <h3>WebMD reviews for Generic Semaglutide®</h3>
+          <Opinion
+            reviewTitle="“Semaglutide Has Revolutionized My Journey to Health in My 70s”"
+            review="The transformation has been incredibly profound. At 78 and 76 years old, my spouse and I have experienced a significant resurgence in our health and vitality thanks to semaglutide. It’s been a cornerstone in our weight loss journey, and we’re living proof that it’s never too late to start. If you’re hesitating, don’t. The change it brings is worth every moment. Wishing you all the best as you rediscover your health and joy in life!”"
+            name="— Old Sparky"
+          />
+        </div>
       </div>
 
 
-      <Footer/>
+      <Footer />
     </div>
   );
 };
--- src/components/Opinion.tsx
+++ src/components/Opinion.tsx
@@ -1,18 +1,11 @@
-export function Opinion() {
-  return (<div>
-    <h2>Believe the Hype:</h2>
-    <h2>Customer Success with Clinically</h2>
-    <h2>Proven Meds</h2>
-    <h3>WebMD reviews for Generic Semaglutide®</h3>
+export function Opinion(props: any) {
+  const { reviewTitle,review, name } = props;
+  return (
     <div>
-      <p>
-        “Semaglutide Has Revolutionized My Journey to Health in My 70s”
-      </p>
+      <p>{reviewTitle}</p>
       <img src="starts.png" alt="" />
-      <p>
-        The transformation has been incredibly profound. At 78 and 76 years old, my spouse and I have experienced a significant resurgence in our health and vitality thanks to semaglutide. It’s been a cornerstone in our weight loss journey, and we’re living proof that it’s never too late to start. If you’re hesitating, don’t. The change it brings is worth every moment. Wishing you all the best as you rediscover your health and joy in life!
-      </p>
-      <p>- Old Sparky</p>
+      <p>{review}</p>
+      <p>- {name}</p>
     </div>
-  </div>);
+  );
 }
