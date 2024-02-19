dd5dd508f2938c88ecf0176552048dd65486e2ea
Andres1~ <andrescorredor20@gmail.com>
2024-02-19 00:25:12 -0500

    Refactor Footer component styling and structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,19 @@
 export function Footer() {
-  return (<footer>
-    <img src="logo192.png" alt="logo" />
-    <h3>All trademarks are the property of their respective owners.</h3>
-    <h3>256-Bit Tls Security</h3>
+  return (<footer className="w-full text-center bg-[#1A1759] text-white p-5">
+    <h3 className="text-[15px]">All trademarks are the property of their respective owners.</h3>
+    <h3 className="text-[19px] m-4">256-Bit Tls Security</h3>
     <h3>Terms of Service</h3>
-    <div>
-      <span>
+    <div className="text-[14px] m-2">
+      <span className="m-2">
         Terms of Use
       </span>
-      <span>
+      <span className="m-2">
         Privacy Policy
       </span>
-      <span>
+      <span className="m-2">
         Consent to Telehealth
       </span>
     </div>
-    <h3>Physician Code of Conduct</h3>
+    <h3 className="text-[14px]">Physician Code of Conduct</h3>
   </footer>);
 }
