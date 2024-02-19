6b6b99325641b90c35c2ecc6ab78868e10c76bde
Andres1~ <andrescorredor20@gmail.com>
2024-02-19 00:56:36 -0500

    Update Opinion and Opinions components

--- src/components/Opinion.tsx
+++ src/components/Opinion.tsx
@@ -1,11 +1,12 @@
 export function Opinion(props: any) {
   const { reviewTitle,review, name } = props;
   return (
-    <div>
-      <p>{reviewTitle}</p>
+    // justify text
+    <div className="text-justify py-[40px]">
+      <p className="font-medium pb-[10px] text-[19px]">{reviewTitle}</p>
       <img src="starts.png" alt="" />
-      <p>{review}</p>
-      <p>- {name}</p>
+      <p className="text-[17px] pt-[20px]">{review}</p>
+      <p className="pt-[20px]">- {name}</p>
     </div>
   );
 }
--- src/components/Opinions.tsx
+++ src/components/Opinions.tsx
@@ -1,11 +1,12 @@
 import { Opinion } from "./Opinion";
 
 export function Opinions() {
-  return (<div className="bg-[#F7F7F7] text-center p-[64px] font-azo">
-    <h2 className="font-bold">Believe the Hype:</h2>
-    <h2 className="font-bold">Customer Success with Clinically</h2>
-    <h2 className="font-bold">Proven Meds</h2>
-    <h3 className="font-bold">WebMD reviews for Generic Semaglutide®</h3>
+  // font color #1A1759
+  return (<div className="bg-[#F7F7F7] text-center px-[64px] py-[5px] font-azo font-normal text-[#1A1759]">
+    <h2 className="font-bold text-[22px]">Believe the Hype:</h2>
+    <h2 className="font-bold text-[22px]">Customer Success with Clinically</h2>
+    <h2 className="font-bold text-[22px]">Proven Meds</h2>
+    <h3 className=" text-[18px]">WebMD reviews for Generic Semaglutide®</h3>
     <Opinion reviewTitle="“Semaglutide Has Revolutionized My Journey to Health in My 70s”" review="The transformation has been incredibly profound. At 78 and 76 years old, my spouse and I have experienced a significant resurgence in our health and vitality thanks to semaglutide. It’s been a cornerstone in our weight loss journey, and we’re living proof that it’s never too late to start. If you’re hesitating, don’t. The change it brings is worth every moment. Wishing you all the best as you rediscover your health and joy in life!”" name="— Old Sparky" />
   </div>);
 }
