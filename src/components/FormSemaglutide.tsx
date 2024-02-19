00f744120ce6f8fb2b77bbc40682f5c0811eeb67
Andres1~ <andrescorredor20@gmail.com>
2024-02-18 20:54:35 -0500

    Update styles and fonts in FormSemaglutide component

--- src/components/FormSemaglutide.tsx
+++ src/components/FormSemaglutide.tsx
@@ -1,7 +1,7 @@
 import { Form } from "@quillforms/renderer-core";
 export 
 function FormSemaglutide() {
-  return (<div className="w-full h-[32rem]">
+  return (<div className="w-full h-[32rem] w-[38rem]">
     <Form formId={1} applyLogic={true} formObj={{
       blocks: [{
         name: "welcome-screen",
@@ -99,19 +99,56 @@ function FormSemaglutide() {
         disableProgressBar: false
       },
       theme: {
-        font: "Roboto",
+        font: "Charter",
+        fontSize: {
+            lg: "5.0rem",
+            sm: "4.0rem"
+        },
         buttonsBgColor: "#190053",
         logo: {
           src: ""
         },
         questionsColor: "#000",
+        questionsLabelFontSize: {
+            lg: "2.5rem",
+            sm: "1.5rem"
+        },
+        questionsLabelLineHeight: {
+            lg: "3.5rem",
+            sm: "2.5rem"
+        },
         answersColor: "#0aa7c2",
         buttonsFontColor: "#fff",
-        buttonsBorderRadius: 25,
+        buttonsFontSize: {
+            lg: "1rem",
+            sm: "1.0rem"
+        },
+        buttonsBorderRadius: 59,
         errorsFontColor: "#fff",
         errorsBgColor: "#f00",
         progressBarFillColor: "#000",
         progressBarBgColor: "#ccc",
+        buttonsPadding: {
+            top: {
+                lg: "1.0rem",
+                sm: "1rem"
+            },
+            bottom: {
+                lg: "1.0rem",
+                sm: "1rem"
+            },
+            left: {
+                lg: "10rem",
+                sm: "1.5rem"
+            },
+            right: {
+                lg: "10rem",
+                sm: "1.5rem"
+            }
+
+        }
+        
+
       },
       messages: {
         'label.hintText.enter':''
