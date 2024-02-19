501f32a5521784475bdaff9cd54cce6e409f71ff
Andres1~ <andrescorredor20@gmail.com>
2024-02-18 23:00:33 -0500

    Refactor API response interceptor

--- src/APIModule.tsx
+++ src/APIModule.tsx
@@ -3,13 +3,10 @@ import { mockData } from './api/mockData';
 
 const mockAxios: AxiosInstance = axios.create();
 
-// Configuración del interceptor de respuesta
 mockAxios.interceptors.response.use(
   (response: AxiosResponse) => {
-    // Verifica si la respuesta es satisfactoria y contiene datos
-    // Si no contiene datos (por ejemplo, un array vacío o un objeto vacío), retorna los datos del mock
     if (!response.data || Object.keys(response.data).length === 0) {
-      console.log("No se encontraron datos. Retornando datos del mock.");
+      console.log("No data found. Returning mock data.");
       return Promise.resolve({
         data: mockData,
         status: 200,
@@ -18,13 +15,10 @@ mockAxios.interceptors.response.use(
         config: response.config,
       });
     }
-    // Si la respuesta es satisfactoria y contiene datos, retorna la respuesta original
     return response;
   }, 
   (error) => {
-    // En caso de error en la solicitud (por ejemplo, error de red o error del servidor),
-    // también puedes optar por retornar los datos del mock
-    console.log("Error en la solicitud. Retornando datos del mock.");
+    console.log("Request error. Returning mock data.");
     return Promise.resolve({
       data: mockData,
       status: 200,
@@ -32,8 +26,6 @@ mockAxios.interceptors.response.use(
       headers: {},
       config: error.config,
     });
-    // O si prefieres manejar el error de otra manera, descomenta la siguiente línea
-    // return Promise.reject(error);
   }
 );
 
